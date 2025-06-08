9500ad4d1ca602b36a731f079de58cb69fa158b9
André Wozniack <84557148+AndreWozniack@users.noreply.github.com>
2025-06-08 20:44:19 -0300

    Updated controller to receive and treat api use cases

--- backend/app/Http/Controllers/Api/DomainController.php
+++ backend/app/Http/Controllers/Api/DomainController.php
@@ -3,38 +3,44 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\DomainCollection;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Http\JsonResponse;
+use App\Services\DomainService;
+use App\Http\Requests\DomainRequest;
+use App\Http\Resources\DomainResource;
+use App\Models\Domain;
 
 class DomainController extends Controller
 {
-    public function __construct(private DomainService $service) {}
+    public function __construct(private readonly DomainService $service) {}
 
-    public function index(): JsonResponse
+    public function index(): DomainCollection
     {
-        return response()->json($this->service->list());
+        return new DomainCollection($this->service->list());
     }
 
-    public function store(DomainRequest $req) : JsonResponse
+    public function store(DomainRequest $req): JsonResponse
     {
-        $domain = $this->service->create($req->validated());
-        return response()->json($domain, 201);
+        $dominio = $this->service->create($req->validated());
+        return response()->json(new DomainResource($dominio), 201);
     }
 
-    public function show(Domain $dominio): Domain
+    public function show(Domain $domain): DomainResource
     {
-        return $dominio; // route-model-binding
+        return new DomainResource($domain);
     }
 
-    public function update(DomainRequest $req, Domain $dominio)
+    public function update(DomainRequest $req, Domain $domain): DomainResource
     {
-        return $this->service->update($dominio, $req->validated());
+        $updated = $this->service->update($domain, $req->validated());
+        return new DomainResource($updated);
     }
 
-    public function destroy(Domain $dominio): Response
+    public function destroy(Domain $domain): Response
     {
-        $this->service->delete($dominio);
+        $this->service->delete($domain);
         return response()->noContent();
     }
 }
