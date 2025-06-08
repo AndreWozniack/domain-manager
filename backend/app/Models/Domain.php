14e31d0b8f08b5a55e3224be26f5e97f7bc98fcb
André Wozniack <84557148+AndreWozniack@users.noreply.github.com>
2025-06-08 20:43:40 -0300

    Model fixed for creating and saving data

--- backend/app/Models/Domain.php
+++ backend/app/Models/Domain.php
@@ -10,11 +10,31 @@ class Domain extends Model
     /** @use HasFactory<\Database\Factories\DomainFactory> */
     use HasFactory;
 
+    protected $fillable = [
+        'nome',
+        'dominio',
+        'cliente',
+        'ativo',
+        'data_registro',
+        'data_expiracao',
+        'observacoes',
+    ];
+
+    protected $casts = [
+        'data_registro' => 'date',
+        'data_expiracao' => 'date',
+        'ativo' => 'boolean',
+    ];
     public static function find(int $id)
     {
+        return self::query()->find($id);
     }
 
-    public static function create(array $d)
+    public static function create(array $data): Domain
     {
+        $domain = new self();
+        $domain->fill($data);
+        $domain->save();
+        return $domain;
     }
 }
