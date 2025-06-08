0503228b29a402d202e23c2b64cf42f27414881b
André Wozniack <84557148+AndreWozniack@users.noreply.github.com>
2025-06-08 20:39:24 -0300

    Update DomainFactory.php

--- backend/database/factories/DomainFactory.php
+++ backend/database/factories/DomainFactory.php
@@ -2,22 +2,24 @@
 
 namespace Database\Factories;
 
+use App\Models\Domain;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Domain>
- */
 class DomainFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
+
+    protected $model = Domain::class;
+
     public function definition(): array
     {
         return [
-            //
+            'nome'           => $this->faker->company,
+            'dominio'        => $this->faker->unique()->domainName,
+            'cliente'        => $this->faker->company,
+            'ativo'          => $this->faker->boolean(95),
+            'data_registro'  => $this->faker->dateTimeBetween('-5 years'),
+            'data_expiracao' => $this->faker->dateTimeBetween('now', '+3 years'),
+            'observacoes'    => $this->faker->optional()->sentence,
         ];
     }
 }
